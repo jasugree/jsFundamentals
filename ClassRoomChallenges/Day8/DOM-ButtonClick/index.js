717acd86e0aab685a377aea7e281206725361b30
Jake Greene <jsgreene208@gmail.com.com>
2021-06-15 19:47:14 -0400

    first commit

--- ClassRoomChallenges/Day8/DOM-ButtonClick/index.js
+++ ClassRoomChallenges/Day8/DOM-ButtonClick/index.js
@@ -1,27 +1,64 @@
-let team1points = document.getElementById('team1points');
+// let team1points = document.getElementById('team1points');
 
-console.log(team1points.innerText);
-console.log(typeof team1points.innerText);
+// console.log(team1points.innerText);
+// console.log(typeof team1points.innerText);
 
-function initializePoints(){
-    team1points.innerText = '0';
+// function initializePoints(){
+//     team1points.innerText = '0';
 
+// }
+
+// initializePoints();
+
+// function team1Add(){
+//     let points = team1points.innerText
+//     points = +points + 1
+//     team1points.innerText = points
+// }
+
+
+// function team1Subtract(){
+//     let points = team1points.innerText
+//     if(points !== '0'){
+//     points = +points - 1
+//     team1points.innerText = points
+//     }else{alert(`Don't go negative`)}
+// }
+
+
+let teamName = document.querySelectorAll('.teamName');
+let points = document.querySelectorAll('.teamPoints')
+
+function calculatePoints(teamName, points){
+    if(teamName = 'Team 1'){
+        function team1Add(){
+            let points = team1points.innerText
+            points = +points + 1
+            team1points.innerText = points
+        };
+        
+        function team1Subtract(){
+            let points = team1points.innerText
+            if(points !== '0'){
+            points = +points - 1
+            team1points.innerText = points
+            }else{alert(`Don't go negative`)}
+        }
+    }else{
+        function team1Add(){
+            let points = team1points.innerText
+            points = +points + 1
+            team1points.innerText = points
+        };
+        
+        function team1Subtract(){
+            let points = team1points.innerText
+            if(points !== '0'){
+            points = +points - 1
+            team1points.innerText = points
+            }else{alert(`Don't go negative`)}
+        }
+    }
+    team1Add();
+    team1Subtract();
 }
-
-initializePoints();
-
-function team1Add(){
-    let points = team1points.innerText
-    points = +points + 1
-    team1points.innerText = points
-}
-
-
-function team1Subtract(){
-    let points = team1points.innerText
-    if(points !== '0'){
-    points = +points - 1
-    team1points.innerText = points
-    }else{alert(`Don't go negative`)}
-}
-
