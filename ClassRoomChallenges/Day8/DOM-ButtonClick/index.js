8269e847ea7ffa7117453426905cc43c4a8053b2
Jake Greene <jsgreene208@gmail.com.com>
2021-06-15 21:04:26 -0400

    Trying to Update

--- ClassRoomChallenges/Day8/DOM-ButtonClick/index.js
+++ ClassRoomChallenges/Day8/DOM-ButtonClick/index.js
@@ -11,9 +11,9 @@
 // initializePoints();
 
 // function team1Add(){
-//     let points = team1points.innerText
-//     points = +points + 1
-//     team1points.innerText = points
+    // let points = team1points.innerText
+    // points = +points + 1
+    // team1points.innerText = points
 // }
 
 
@@ -26,39 +26,9 @@
 // }
 
 
-let teamName = document.querySelectorAll('.teamName');
-let points = document.querySelectorAll('.teamPoints')
 
-function calculatePoints(teamName, points){
-    if(teamName = 'Team 1'){
-        function team1Add(){
-            let points = team1points.innerText
-            points = +points + 1
-            team1points.innerText = points
-        };
-        
-        function team1Subtract(){
-            let points = team1points.innerText
-            if(points !== '0'){
-            points = +points - 1
-            team1points.innerText = points
-            }else{alert(`Don't go negative`)}
-        }
-    }else{
-        function team1Add(){
-            let points = team1points.innerText
-            points = +points + 1
-            team1points.innerText = points
-        };
-        
-        function team1Subtract(){
-            let points = team1points.innerText
-            if(points !== '0'){
-            points = +points - 1
-            team1points.innerText = points
-            }else{alert(`Don't go negative`)}
-        }
+function calcutlatePoints(teamName, points){
+    if(teamName ="Team 1"){
+        console.log
     }
-    team1Add();
-    team1Subtract();
 }
--- ClassRoomChallenges/Day8/DOM-Challenge/index.js
+++ ClassRoomChallenges/Day8/DOM-Challenge/index.js
@@ -17,3 +17,47 @@ function changeTitle(){
 }
 
 changeTitle();
+
+/* 
+SILVER - 
+In your html file, create a div element underneath your h1 element
+with an id of "div".
+In your js file, grab your div element by its id. Still in your js
+file, create a new paragraph element. Inside the paragraph element
+put in a dummy text (Lorem ipsum). Change the color of the paragraph
+text to: "#6495ED". Nest the paragraph element inside the div
+element.
+*/
+let div = document.getElementById('div');
+let para = document.createElement('p');
+
+para.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+para.style.color = "#6495ED";
+div.append(para);
+
+
+/* 
+GOLD - 
+In your html file underneath your div element, create an unorganized list
+element with an id of "list".
+In your js file, grab your ul element by its id. 
+Create an array in your js file with 4 different items for your to-do
+list. 
+Now use a method to go over all the array items. For each item, 
+create an li element and put the value of that item into your li element. Append 
+the li element to the ul element of "list".
+*/
+
+
+let list = document.getElementById('list');
+
+let arr = ['1 thing', '2 thing', '3 thing', '4 thing'];
+
+let li = document.createElement('li');
+
+
+arr.forEach((item) => {
+    let li = document.createElement('li')
+    li.innerText = item;
+    list.append(li);
+});
